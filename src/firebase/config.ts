a17c6f86f3be05f8490cf8455e1162646c23f0c1
777喵喵喵 <QIQI_MagicCity@protonmail.com>
2025-10-30 02:01:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9804216494-c2b75",
+  "appId": "1:7191046993:web:1bfb1315c72e7ee0bd4170",
+  "apiKey": "AIzaSyDTT1tko3a_QqyZUm_BBERWM6fO2S8iLUE",
+  "authDomain": "studio-9804216494-c2b75.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "7191046993"
+};
