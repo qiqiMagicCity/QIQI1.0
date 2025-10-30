48ab20c3daf3f8388d8bb444d32459f06b88fe52
777喵喵喵 <QIQI_MagicCity@protonmail.com>
2025-10-30 03:47:04 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  devIndicators: {
+    allowedDevOrigins: [
+        'https://6000-firebase-studio-1761782845873.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev'
+    ]
+  }
 };
 
 export default nextConfig;
