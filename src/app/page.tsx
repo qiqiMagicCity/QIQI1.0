d168c34216d1f70b88c382c915612c1578b710b9
777喵喵喵 <QIQI_MagicCity@protonmail.com>
2025-10-30 00:35:48 +0000

    更换页面背景色  为绿色

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+    <div className="flex min-h-screen w-full flex-col">
       <DashboardHeader />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 lg:p-10">
         <div className="flex items-center gap-3">
